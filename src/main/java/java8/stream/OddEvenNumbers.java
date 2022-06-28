011570f552df2349d7f45b178139beb5bb9d02a5
Shivaji Deotale <shivadeotale@gmail.com>
2022-06-28 18:38:20 +0530

    first commit

--- src/main/java/java8/stream/OddEvenNumbers.java
+++ src/main/java/java8/stream/OddEvenNumbers.java
@@ -11,14 +11,14 @@ public class OddEvenNumbers {
 
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
-		// with Stream filter
+		// with Stream.filter
 		List<Integer> odd = numbers.stream().filter(n -> n % 2 != 0).collect(Collectors.toList());
 		System.out.println(odd);
 
 		List<Integer> even = numbers.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
 		System.out.println(even);
 
-		// without Stream filter. using Collectors.partitioningBy
+		// without Stream.filter, using Collectors.partitioningBy
 		Map<Boolean, List<Integer>> response = numbers.stream().collect(Collectors.partitioningBy(n -> n % 2 == 0));
 		System.out.println(response);
 	}
