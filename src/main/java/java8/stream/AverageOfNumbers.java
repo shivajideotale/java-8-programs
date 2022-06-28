bd160879a8d0611f6620b5fc642b60f6be2be69c
Shivaji Deotale <shivadeotale@gmail.com>
2022-06-28 18:27:13 +0530

    first commit

--- src/main/java/java8/stream/AverageOfNumbers.java
+++ src/main/java/java8/stream/AverageOfNumbers.java
@@ -1,18 +1,28 @@
 package java8.stream;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AverageOfNumbers {
 
 	public static void main(String[] args) {
 
-		// Stream.mapToInt
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+		// Stream.mapToInt
 		OptionalDouble averageOfNumbers = numbers.stream().mapToInt(n -> n).average();
 		System.out.println(averageOfNumbers.getAsDouble());
+
+		// IntSummaryStatistics
+		IntSummaryStatistics stats = numbers.stream().mapToInt(n -> n).summaryStatistics();
+		System.out.println(stats.getAverage());
+
+		IntSummaryStatistics stats1 = numbers.stream().collect(Collectors.summarizingInt(n -> n));
+		System.out.println(stats1.getAverage());
 
 		// Array.stream
 		int[] array = new int[] { 1, 2, 3, 4, 5 };
