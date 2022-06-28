96d3dd96e4cccf9daed9ca85010731e5668340eb
Shivaji Deotale <shivadeotale@gmail.com>
2022-06-28 09:07:19 +0530

    first commit

--- src/main/java/java8/stream/SumOfNumbers.java
+++ src/main/java/java8/stream/SumOfNumbers.java
@@ -13,21 +13,24 @@ public class SumOfNumbers {
 		List<Integer> numbers = Arrays.asList(5, 3, 4, 1, 3, 7, 2, 9, 9, 4);
 
 		// Stream.reduce
-		Optional<Integer> sum1 = numbers.stream().reduce((a, b) -> a + b);
-		System.out.println(sum1.get());
-		
-		Optional<Integer> sum2 = numbers.stream().reduce(Integer::sum);
-		System.out.println(sum2.get());
-		
+		Optional<Integer> sumByReduce1 = numbers.stream().reduce((a, b) -> a + b);
+		System.out.println(sumByReduce1.get());
+
+		Optional<Integer> sumByReduce2 = numbers.stream().reduce(Integer::sum);
+		System.out.println(sumByReduce2.get());
+
+		Integer sumByReduce3 = numbers.stream().reduce(0, Integer::sum);
+		System.out.println(sumByReduce3);
+
+		Integer sumByReduce4 = numbers.stream().reduce(0, Integer::sum, Integer::sum);
+		System.out.println(sumByReduce4);
 
 		// IntStream.sum
 		IntStream intStream1 = numbers.stream().mapToInt(n -> n);
-		int sum3 = intStream1.sum();
-		System.out.println(sum3);
-		
+		System.out.println(intStream1.sum());
+
 		IntStream intStream2 = numbers.stream().mapToInt(Integer::intValue);
-		int sum4 = intStream2.sum();
-		System.out.println(sum4);
+		System.out.println(intStream2.sum());
 
 		// Collectors.summingInt
 		int sum5 = numbers.stream().collect(Collectors.summingInt(n -> n));
